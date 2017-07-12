0588f7cbfd6fc8d69a60805ea50fb8407d88e9e6
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-07-12 20:08:28 +0100

    Refactor routes

--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -10,14 +10,12 @@ class Routes extends React.Component {
       <Switch>
         <Redirect exact from="/" to="/reach/countries" />
         <Redirect exact from="/reach" to="/reach/countries" />
-        <Route exact path="/reach/countries" component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
-        <Route exact path="/reach/regions" component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
-        <Route exact path="/reach/countries/:country" component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
-        <Route exact path="/reach/regions/:region" component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
-        <Route exact path="/impact" component={(props) => <AppWrapper mainView="impact" {...props} />} />
-        <Route exact path="/impact/:region" component={(props) => <AppWrapper mainView="impact" {...props} />} />
+
+        <Route exact path="/reach/countries/:country?" component={(props) => <AppWrapper mainView="reach" subView="countries" {...props} />} />
+        <Route exact path="/reach/regions/:region?" component={(props) => <AppWrapper mainView="reach" subView="regions" {...props} />} />
         <Route exact path="/impact/:country/story/:story" component={(props) => <AppWrapper mainView="impact" {...props} />} />
-        <Route exact path="/impact/:region/:country" component={(props) => <AppWrapper mainView="impact" {...props} />} />
+        <Route exact path="/impact/:region?/:country?" component={(props) => <AppWrapper mainView="impact" {...props} />} />
+
         <Route exact path="*" component={(props) => <AppWrapper mainView="notfound" {...props} />} />
       </Switch>
     </Router>);
