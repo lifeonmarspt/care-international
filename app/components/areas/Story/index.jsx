4ce0a713ef0dd720c328b7768dcdc8f18e05ee4f
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-07-17 17:44:46 +0100

    Implement story mobile layout

--- app/components/areas/Story/index.jsx
+++ app/components/areas/Story/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import programs from "resources/programs.json";
 import momImage from "images/mom.png";
 import "./style.scss";
 
@@ -13,12 +14,24 @@ class Story extends React.Component {
 
   render() {
     return (<div id="story">
+      <div className="show-mobile">
+        <ul className="mobile-title">
+          <li>
+            <div className="story-hide" onClick={this.props.handleCloseStory} />
+          </li>
+          <li>
+            {this.props.story.story}
+          </li>
+        </ul>
+      </div>
       <div className="img-wrapper">
         <img src={momImage} alt="Mom" />
       </div>
       <div className="close-button" onClick={this.props.handleCloseStory} />
       <div className="content">
-        <h1>{this.props.story.story}</h1>
+        <h1 className="show-desktop">{this.props.story.story}</h1>
+        <h2 className="show-mobile">{programs.find((p) => p.id === this.props.story.outcome).label}</h2>
+        <h3 className="show-mobile">{this.props.story.country}</h3>
         <hr />
       </div>
     </div>);
